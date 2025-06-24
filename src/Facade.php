201c5586fbb8f5c22b25e0ad61f1765f0dfbec4e
Korneel Eeckhout <korneel@pienter.tech>
2025-06-24 13:02:07 +0200

    Update phpdocs

--- src/Facade.php
+++ src/Facade.php
@@ -4,6 +4,14 @@ namespace Oak;
 
 use Oak\Contracts\Container\ContainerInterface;
 
+/**
+ * Base Facade class providing static access to services in the container
+ * 
+ * Facades act as static proxies to underlying service instances, providing
+ * a clean, expressive interface while maintaining the benefits of dependency injection.
+ * 
+ * @template T of object
+ */
 abstract class Facade
 {
     /**
@@ -14,25 +22,28 @@ abstract class Facade
     private static $container;
 
     /**
-     * Sets the container to use for creating contracts
+     * Sets the container to use for resolving service contracts
      *
-     * @param ContainerInterface $container
+     * @param ContainerInterface $container The container instance
+     * @return void
      */
-    public static function setContainer(ContainerInterface $container)
+    public static function setContainer(ContainerInterface $container): void
     {
         self::$container = $container;
     }
 
     /**
-     * @return string
+     * Get the service contract/class name that this facade represents
+     * 
+     * @return class-string<T> The fully qualified class name or interface
      */
     abstract protected static function getContract(): string;
 
     /**
-     * Gets the instance for this facade
+     * Resolve and return the service instance from the container
      *
-     * @return mixed
-     * @throws \Exception
+     * @return T The resolved service instance
+     * @throws \Exception When no container is set or service cannot be resolved
      */
     final protected static function getInstance()
     {
@@ -44,14 +55,16 @@ abstract class Facade
     }
 
     /**
-     * Statically call the requested method
-     *
-     * @param $method
-     * @param $arguments
-     * @return mixed
-     * @throws \Exception
+     * Handle static method calls by forwarding them to the resolved service instance
+     * 
+     * This magic method enables static access to instance methods on the underlying service.
+     * 
+     * @param string $method The method name to call
+     * @param array $arguments The arguments to pass to the method
+     * @return mixed The result of the method call
+     * @throws \Exception When the service cannot be resolved or method doesn't exist
      */
-    public static function __callStatic($method, $arguments)
+    public static function __callStatic(string $method, array $arguments)
     {
         return static::getInstance()->$method(...$arguments);
     }

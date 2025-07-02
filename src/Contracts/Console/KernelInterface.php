e3bee2ebc54c2e5324ffbcac3ee9d0bdffb251fb
Korneel Eeckhout <korneel@pienter.tech>
2025-07-02 15:44:47 +0200

    Update the console/KernelInterface to mirror the console/Kernel implementation

--- src/Contracts/Console/KernelInterface.php
+++ src/Contracts/Console/KernelInterface.php
@@ -3,10 +3,22 @@
 namespace Oak\Contracts\Console;
 
 /**
- * Interface KernerInterface
+ * Interface KernelInterface
  * @package Oak\Contracts\Console
  */
 interface KernelInterface
 {
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @throws \Exception
+     */
     public function handle(InputInterface $input, OutputInterface $output);
+
+    /**
+     * Register a command
+     *
+     * @param $command
+     */
+    public function registerCommand($command);
 }

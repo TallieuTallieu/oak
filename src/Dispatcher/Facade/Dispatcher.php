e5760f55ccad2ba37e430679fd9fa58dfa5e3cde
Korneel Eeckhout <korneel@pienter.tech>
2025-10-15 14:09:41 +0200

    fix: use fully qualified EventInterface in Dispatcher facade docblock

--- src/Dispatcher/Facade/Dispatcher.php
+++ src/Dispatcher/Facade/Dispatcher.php
@@ -7,17 +7,17 @@ use Oak\Facade;
 
 /**
  * Dispatcher Facade providing static access to event dispatching functionality
- * 
+ *
  * @method static mixed addListener(string $eventName, callable $listener) Add an event listener
  * @method static array getListeners(string $eventName) Get all listeners for an event
  * @method static bool hasListeners(string $eventName) Check if an event has listeners
- * @method static mixed dispatch(string $eventName, EventInterface|null $event = null) Dispatch an event to listeners
+ * @method static mixed dispatch(string $eventName, \Oak\Contracts\Dispatcher\EventInterface|null $event = null) Dispatch an event to listeners
  */
 class Dispatcher extends Facade
 {
     /**
      * Get the service contract that this facade represents
-     * 
+     *
      * @return class-string<DispatcherInterface>
      */
     protected static function getContract(): string
